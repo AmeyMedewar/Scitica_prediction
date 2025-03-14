e5e4d2328602f10066c7038fc1c2d61760c47db0
Amey Medewar <121146815+AmeyMedewar@users.noreply.github.com>
2025-03-14 11:41:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open("model.pkl", "rb") as f:
+with open("model(1).pkl", "rb") as f:
     model = pickle.load(f)
 
 # Set page config
